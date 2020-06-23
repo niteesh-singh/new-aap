55ba23032ef22705906fa5c361ec54f735532065
Niteesh Singh <niteeshsingh5@gmail.com>
2020-06-23 14:10:53 +0530

    Update to deploy to heroku

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const ngApp = express();
 
-ngApp.use(express.static('./dist/herokuApp'));
+ngApp.use(express.static(__dirname, './dist/heroku-app'));
 
 ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/herokuApp/index.html'));
+    response.sendFile(path.join(__dirname, '/dist/heroku-app/index.html'));
 });
 
 ngApp.listen(process.env.PORT || 8080);
