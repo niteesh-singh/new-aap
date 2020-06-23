721ea0ec6b215e9585a6639ed654907495ec2513
Niteesh Singh <niteeshsingh5@gmail.com>
2020-06-23 14:55:02 +0530

    Change in server.js file

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const ngApp = express();
 
-ngApp.use(express.static(__dirname, './dist/heroku-app'));
+ngApp.use(express.static(__dirname, './dist/herokuApp'));
 
 ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/heroku-app/index.html'));
+    response.sendFile(path.join(__dirname, '/dist/herokuApp/index.html'));
 });
 
 ngApp.listen(process.env.PORT || 8080);
