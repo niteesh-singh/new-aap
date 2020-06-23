f3bf3928e86e8217afc05524782bda9b81a197f5
Niteesh Singh <niteeshsingh5@gmail.com>
2020-06-23 16:52:26 +0530

    test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const ngApp = express();
 
-ngApp.use(express.static(__dirname, './dist/herokuApp'));
+ngApp.use(express.static(__dirname + '/dist/herokuApp'));
 
 ngApp.get('/*', function (request, response) {
     response.sendFile(path.join(__dirname, '/dist/herokuApp/index.html'));
