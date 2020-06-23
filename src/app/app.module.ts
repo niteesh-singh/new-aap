ff12adf64699047537a517efa27327419290cd92
Niteesh Singh <niteeshsingh5@gmail.com>
2020-06-23 18:50:02 +0530

    Added components and their routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ElementsModule } from './elements/elements.module';
+import { CollectionsModule } from './collections/collections.module';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ElementsModule,
+    CollectionsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
